f87988912f8b3dafa8e9319413deeea8115828ab
Ale Bottini <alebottini002@gmail.com>
2021-01-29 19:15:09 -0300

    first part

---
+++ twotter/src/components/UserProfile.vue
@@ -0,0 +1,105 @@
+<template>
+  
+  <div class="user-profile">
+    <div class="user-profile_user-panel">
+        <h1 class = "user-profile_username">@{{ user.username }}</h1>          
+        <div class= "user-profile__admin-badge" v-if="user.isAdmin">
+          Admin
+        </div>
+        <div class= "user-profile_follower-count">
+          <strong> Followers: </strong> {{ followers }}
+        </div>
+    </div>     
+    <div class= "user-profile_twoots-wrapper">
+        <div class= "user-profile_twoot" v-for="twoot in user.twoots" :key="twoot.id">
+          {{ twoot.content }}
+        </div>
+    </div>
+  </div>   
+</template>
+
+<script>
+
+export default {
+  name: 'UserProfile',
+  data(){
+    return {
+
+      followers: 0,
+      user: {
+        id : 1,
+        username: 'aalebottini',
+        firstName: 'ale',
+        lastName: 'B',
+        email: 'alebottini002@gmail.com',
+        isAdmin: true,
+        twoots: [
+          {id: 1, content: "wlcome to twotter"},
+          {id: 2, content: "Dont be afraid"}
+        ]
+      }
+    }
+   },
+   watch: {
+     followers (newFollowerCount, oldFolloweCount){
+       if (oldFolloweCount < newFollowerCount) {
+         console.log (`${this.user.username} has gained a follower`)
+       }
+     }
+
+   },
+    computed: {
+      fullName() {
+        return `${this.user.firstName} ${this.user.lastName}`;
+
+      }
+  },
+  methods: {
+    followUser(){
+      this.followers++
+    }
+  },
+  mounted (){
+    this.followUser()
+  }
+}
+
+</script>
+
+<style>
+.user-profile{
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
+
+.user-profile_user-panel{
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #0FE3E8;
+  
+}
+
+.user-profile__admin-badge{
+  background: rebeccapurple;
+  color: white;
+  padding:0 10px;
+  border-radius: 5px;
+  margin-right: auto;
+  font-weight: bold;
+
+}
+
+.user-profile_username{
+  margin: 5px;
+}
+
+.user-profile_follower-count{
+  margin-right:auto;
+}
+</style>
